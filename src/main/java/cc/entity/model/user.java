5eb55ab883e6d469e34662cd821027917f0f8173
suqing <1603413478@qq.com>
2018-11-06 23:20:20 +0800

    user entity

---
+++ src/main/java/cc/entity/model/user.java
@@ -0,0 +1,63 @@
+package cc.entity.model;
+
+public class user {
+    private Integer id;
+
+    private String userName;
+
+    private String password;
+
+    private String name;
+
+    private Integer sex;
+
+    private String flag;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag == null ? null : flag.trim();
+    }
+}
